b6cffb01043f8e3bb6fa3a0bd39af0d5a70e2508
Eric <2549462929@qq.com>
2017-11-26 18:40:01 +0800

    strategy-pattern commit

---
+++ src/main/java/com/github/eric/strategy/strategy_pattern/QuackWithGuaGua.java
@@ -0,0 +1,9 @@
+package com.github.eric.strategy.strategy_pattern;
+
+public class QuackWithGuaGua implements Quack{
+
+	@Override
+	public void quack() {
+		System.out.println("火箭鸭可以发出火箭般的叫声");
+	}
+}
