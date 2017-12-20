6f96f6446dfe4755eea90f458a763245334e9778
Eric <2549462929@qq.com>
2017-12-20 22:40:30 +0800

    singleton-pattern commit

---
+++ src/main/java/com/github/eric/strategy/singleton_pattern/SynchronizedSingleton.java
@@ -0,0 +1,17 @@
+package com.github.eric.strategy.singleton_pattern;
+
+public class SynchronizedSingleton {
+	//只有第一次需要进行new，后面都是直接返回实例，因为已经有了直接返回
+	private static SynchronizedSingleton instance;
+
+	private SynchronizedSingleton() {
+	}
+
+	//线程安全的单例，但是每次都要进行同步判断
+	public synchronized SynchronizedSingleton getInstance(){
+		if (instance==null){
+			instance = new SynchronizedSingleton();
+		}
+		return instance;
+	}
+}
